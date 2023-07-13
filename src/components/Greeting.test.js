3e1105a16b35d5c78e7f72624675c5c81458fd44
aadil42 <adilkelawala@gmail.com>
2023-07-13 09:31:37 +0530

    adding explicit test suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from '@testing-library/react';
 
 import Greeting from './Greeting';
-test('testing Greeting Component', () => {
 
-    // Arrange
-    render(<Greeting />);
+describe('this is greeting suite', () => {
+    test('testing Greeting Component', () => {
 
-    // act
-
-    // assert
-    const helloWorldText = screen.getByText(/hello world!/i);
-    expect(helloWorldText).toBeInTheDocument();
-});
+        // Arrange
+        render(<Greeting />);
+    
+        // act
+    
+        // assert
+        const helloWorldText = screen.getByText(/hello world!/i);
+        expect(helloWorldText).toBeInTheDocument();
+    });
+})
