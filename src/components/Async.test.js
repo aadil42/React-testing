dd1e7771219f1b9bd0df66022e3165f7ab8b90e6
aadil42 <adilkelawala@gmail.com>
2023-07-13 12:25:32 +0530

    stimulating mock request to the server

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,13 @@ import Async from './Async';
 
 describe('Async Component', () => {
     test('renders asnycronously fetched list', async () => {
+
+        // the fetch funciton is only overwritten in this function.
+        // it would stimulate the request without sending it to the server.
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id:'key1', title:'title1'}]
+        });
         // Arrange
         render(<Async />);
 
